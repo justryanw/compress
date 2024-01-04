22589f2f5f49e70cf7cea34200587d6ed645bae6
Ryan Walker <ryanjwalker2001@gmail.com>
2024-01-04 01:51:26 +0000

    name

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
     perSystem = { pkgs, system, ... }: {
       packages.default = pkgs.writeShellScriptBin "compress" ''
         file=$1
+        output="''${1%.*}-$2MB.mp4"
         target_size_mb=$2  # target size in MB
         target_size=$(( $target_size_mb * 1000 * 1000 * 8 )) # target size in bits
         length=`${pkgs.ffmpeg}/bin/ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$file"`
@@ -15,7 +16,7 @@
         total_bitrate=$(( $target_size / $length_round_up ))
         audio_bitrate=$(( 128 * 1000 )) # 128k bit rate
         video_bitrate=$(( $total_bitrate - $audio_bitrate ))
-        ${pkgs.ffmpeg}/bin/ffmpeg -i "$file" -b:v $video_bitrate -maxrate:v $video_bitrate -bufsize:v $(( $target_size / 20 )) -b:a $audio_bitrate "''${file}-''${target_size_mb}mb.mp4"
+        ${pkgs.ffmpeg}/bin/ffmpeg -i "$file" -b:v $video_bitrate -maxrate:v $video_bitrate -bufsize:v $(( $target_size / 20 )) -b:a $audio_bitrate "$output"
       '';
     };
   };
